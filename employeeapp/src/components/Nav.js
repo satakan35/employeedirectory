3f6ce7a1cb5c143cee8fb0a8a65b3092a1528929
Sedat Atakan <satakan35@gmail.com>
2020-07-01 17:25:30 -0400

    updated navjs

--- employeeapp/src/components/Nav.js
+++ employeeapp/src/components/Nav.js
@@ -6,6 +6,33 @@ function Nav({ handleSearchChange }) {
   return (
     <nav className="navbar navbar-expand navbar-light bg-light">
      {/* We render the SearchBox here and pass in necessary props */}
+     <Link className="navbar-brand" to="/">
+        Wikipedia Searcher
+      </Link>
+      <div>
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <Link
+              to="/"
+              className={
+                window.location.pathname === "/" || window.location.pathname === "/home"
+                  ? "nav-link active"
+                  : "nav-link"
+              }
+            >
+              Home
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link
+              to="/signup"
+              className={window.location.pathname === "/signup" ? "nav-link active" : "nav-link"}
+            >
+              Sign Up
+            </Link>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 }
