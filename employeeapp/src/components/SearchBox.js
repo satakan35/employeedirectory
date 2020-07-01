2078ab6dbfd80c3eb01addff2ae909dbad45364a
Sedat Atakan <satakan35@gmail.com>
2020-07-01 17:18:00 -0400

    updated searhbox

--- employeeapp/src/components/SearchBox.js
+++ employeeapp/src/components/SearchBox.js
@@ -3,6 +3,19 @@ function SearchBox({ handleSearchChange }) {
       <div className="searchbox">
         <form className="form-inline">
          {/* this will be the input box for the searchbox */}
+         <label htmlFor="search">Search:</label>
+        <input
+          onChange={props.handleInputChange}
+          value={props.search}
+          name="search"
+          type="text"
+          className="form-control"
+          placeholder="Search for a Gif"
+          id="search"
+        />
+        <button onClick={props.handleFormSubmit} className="btn btn-primary mt-3">
+          Search
+        </button>
         </form>
       </div>
     );
